ee324dbbaf8464f7603458254f57a397fd632466
unknown <gmiller@TX9060N002.HRM.local>
2015-05-14 21:18:06 -0500

    Testing the CCS import of the code project. Nothing changed

--- Firmware/Debug/main.pp
+++ Firmware/Debug/main.pp
@@ -1,13 +1,15 @@
 # FIXED
 
 main.obj: ../main.c
-main.obj: c:/ti/ccsv6/ccs_base/msp430/include/msp430.h
-main.obj: c:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h
-main.obj: c:/ti/ccsv6/ccs_base/msp430/include/in430.h
-main.obj: c:/ti/ccsv6/tools/compiler/msp430_4.3.5/include/intrinsics.h
+main.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h
+main.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics_legacy_undefs.h
 
 ../main.c: 
-c:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
-c:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h: 
-c:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
-c:/ti/ccsv6/tools/compiler/msp430_4.3.5/include/intrinsics.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics_legacy_undefs.h: 
--- Firmware/Debug/subdir_rules.mk
+++ Firmware/Debug/subdir_rules.mk
@@ -6,14 +6,14 @@
 font1.obj: ../font1.c $(GEN_OPTS) $(GEN_HDRS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: MSP430 Compiler'
-	"c:/ti/ccsv6/tools/compiler/msp430_4.3.5/bin/cl430" -vmsp --abi=coffabi --include_path="c:/ti/ccsv6/ccs_base/msp430/include" --include_path="c:/ti/ccsv6/tools/compiler/msp430_4.3.5/include" --advice:power=all -g --define=__MSP430G2553__ --diag_warning=225 --display_error_number --diag_wrap=off --printf_support=minimal --preproc_with_compile --preproc_dependency="font1.pp" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/bin/cl430" -vmsp --abi=coffabi --include_path="C:/ti/ccsv6/ccs_base/msp430/include" --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include" --advice:power=all -g --define=__MSP430G2553__ --diag_warning=225 --display_error_number --diag_wrap=off --printf_support=minimal --preproc_with_compile --preproc_dependency="font1.pp" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 main.obj: ../main.c $(GEN_OPTS) $(GEN_HDRS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: MSP430 Compiler'
-	"c:/ti/ccsv6/tools/compiler/msp430_4.3.5/bin/cl430" -vmsp --abi=coffabi --include_path="c:/ti/ccsv6/ccs_base/msp430/include" --include_path="c:/ti/ccsv6/tools/compiler/msp430_4.3.5/include" --advice:power=all -g --define=__MSP430G2553__ --diag_warning=225 --display_error_number --diag_wrap=off --printf_support=minimal --preproc_with_compile --preproc_dependency="main.pp" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/bin/cl430" -vmsp --abi=coffabi --include_path="C:/ti/ccsv6/ccs_base/msp430/include" --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include" --advice:power=all -g --define=__MSP430G2553__ --diag_warning=225 --display_error_number --diag_wrap=off --printf_support=minimal --preproc_with_compile --preproc_dependency="main.pp" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
